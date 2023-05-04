de5b673f1fa26a4ec5484702f1e863df9b450b96
LeonidTuz <tuz.leonid7@gmail.com>
2023-05-04 04:26:21 +0300

    refactor code

--- Pyrus_client.py
+++ Pyrus_client.py
@@ -3,6 +3,7 @@ import requests
 from pathlib import Path
 from loguru import logger
 from pprint import pprint
+from datetime import datetime
 
 
 class Pyrus:
@@ -30,7 +31,9 @@ class Pyrus:
     def get_register(self, form_id: int):
         response = requests.post(f"https://api.pyrus.com/v4/forms/{form_id}/register",
                                  headers=self.default_headers,
-                                 json={"include_archived": "y"})
+                                 json={"include_archived": "y",
+                                       "field_ids": "3"},
+                                 timeout=1)
         if response.ok:
             logger.info(f"register {form_id} got")
             return response
@@ -42,20 +45,6 @@ class Pyrus:
                                 headers=self.default_headers)
         return response
 
-    def collect_tickets(self, register_id: int):
-        ticket_list: list[dict] = list()
-
-        register = self.get_register(register_id).json()
-        tickets_ids = (x['id'] for x in register['tasks'])
-
-        for ticket in tickets_ids:
-            ticket_list.append(self.get_task(ticket).json())
-            logger.info(f"{ticket} got")
-
-        logger.info(f"collect tickets performed")
-
-        return ticket_list
-
     def get_file(self, file_id: int):
         response = requests.get(f"https://api.pyrus.com/v4/files/download/{file_id}",
                                 headers=self.default_headers)
@@ -64,6 +53,48 @@ class Pyrus:
         else:
             logger.error("get_file don't work")
             pprint(response.__dict__)
+
+    def get_id_and_name(self, register_id):
+        ticket_list: list[dict] = list()
+        register = self.get_register(register_id).json()['tasks']
+        id_value = list()
+        count = 1
+        for field in register:
+            if 'value' not in field['fields'][0]:
+                continue
+            for value in field['fields'][0]['value']:
+                value_dict = dict()
+                value_dict['id'] = value['id']
+                value_dict['name'] = f"{count}{value['name']}"
+                count += 1
+                ticket_list.append(value_dict)
+        logger.info("get id and name list")
+        return ticket_list
+
+    def save_files(self, register_id):
+        id_and_name_list = self.get_id_and_name(register_id)
+        timestamp = datetime.now().strftime('%d.%m.%Y_%H.%M')
+        DIR_NAME = "photos_" + timestamp
+        dir = Path.cwd() / DIR_NAME
+
+        if not dir.exists():
+            dir.mkdir()
+            logger.info("make dir")
+        for value in id_and_name_list:
+            photo = self.get_file(value['id'])
+            file_path = dir / value['name']
+            with open(file_path, "wb") as fp:
+                fp.write(photo['_content'])
+                logger.info(f"save photo {value['name']}")
+
+        my_file = open("last_upload.txt", "w+")
+        my_file.write(f"{timestamp}")
+        my_file.close()
+        logger.info("make last upload info")
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -7,38 +7,7 @@ from loguru import logger
 def main():
     url = Path("creds.txt")
     pyrus_client = Pyrus(url)
-    tickets = pyrus_client.collect_tickets(1203495)
-
-    id_and_name_list: list[dict] = list()
-    for ticket in tickets:
-        if 'value' not in ticket['task']['fields'][5]:
-            continue
-        for value in ticket['task']['fields'][5]['value']:
-            my_dict = dict()
-            my_dict['name'] = value['name']
-            my_dict['id'] = value['id']
-            id_and_name_list.append(my_dict)
-        logger.info(f"Get value from task {ticket['task']['id']}")
-
-    timestamp = datetime.now().strftime('%d.%m.%Y_%H.%M')
-    DIR_NAME = "photos_" + timestamp
-
-    dir = Path.cwd() / DIR_NAME
-
-    if not dir.exists():
-        dir.mkdir()
-        logger.info("make dir")
-    for value in id_and_name_list:
-        photo = pyrus_client.get_file(value['id'])
-        file_path = dir / value['name']
-        with open(file_path, "wb") as fp:
-            fp.write(photo['_content'])
-            logger.info(f"save photo {value['name']}")
-
-    my_file = open("last_upload.txt", "w+")
-    my_file.write(f"{timestamp}")
-    my_file.close()
-    logger.info("make last upload info")
+    pyrus_client.save_files(1203495)
 
 
 if __name__ == "__main__":
